e52c267125380861df6961548aa1681bbc1cbf04
Brian Loveless <Brian.loveless1@yahoo.com>
2020-01-31 12:00:25 -0500

    start

---
+++ fact.js
@@ -0,0 +1,54 @@
+/// show and hide general facts
+
+let info = document.querySelector(".general");
+
+const foot = document.querySelector(".football");
+foot.addEventListener("click", showGen);
+
+function showGen() {
+  info.style.display = "block";
+}
+
+const closeGen = document.querySelector(".closeGen");
+closeGen.addEventListener("click", hideGen);
+
+function hideGen() {
+  info.style.display = "none";
+}
+
+/// show and hide 49 ers facts
+let nine = document.querySelector(".niners");
+
+let san = document.querySelector(".sanfran");
+
+let closesan = document.querySelector(".closeSan");
+
+nine.addEventListener("click", showSan);
+
+function showSan() {
+  san.style.display = "block";
+}
+
+closesan.addEventListener("click", hideSan);
+
+function hideSan() {
+  san.style.disply = "none";
+}
+
+/// show and hide Chiefs facts
+
+let chief = document.querySelector(".chiefs");
+let kc = document.querySelector(".kc");
+let closeKan = document.querySelector(".closeKan");
+
+chief.addEventListener("click", showKan);
+
+function showKan() {
+  kc.style.display = "block";
+}
+
+closeKan.addEventListener("click", hideKan);
+
+function hideKan() {
+  kc.style.display = "none";
+}
